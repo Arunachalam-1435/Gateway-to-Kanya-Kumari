4917562a61f8807786d77130e98d7759f9bb357e
Arunachalam-1435 <karunachalam120@gmail.com>
2026-04-07 11:13:19 +0530

    Router fixed & 404 page added

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -4,21 +4,37 @@ namespace App\Core;
 use App\Controllers\PlaceController;
 use App\Controllers\HotelController;
 class Router{
-    public function dispatch(string $method, string $path, ?string $id):void{
-        switch($path){
+    public function dispatch(string $method, ?string $path1, ?string $path2, ?string $id):void{
+        switch($path1){
             case "home":
                 require __DIR__.'/../../public/pages/index.html';
                 break;
-            case "places":
-                $this->p_controller = new PlaceController();
-                $this->p_controller->placeController($method, $id);
-                break;
-            case 'hotels':
-                $this->h_controller = new HotelController();
-                $this->h_controller->hotelController($method, $id);
-                break;
+            case "api":
+                if(!empty($path2)){
+                    if($path2 == "places"){
+                        header("Content-Type: application/json");
+                        $this->p_controller = new PlaceController();
+                        $this->p_controller->placeController($method, $id);
+                        break;
+                    }
+                    elseif($path2 == "hotels"){
+                        header("Content-Type: application/json");
+                        $this->h_controller = new HotelController();
+                        $this->h_controller->hotelController($method, $id);
+                        break;
+                    }
+                    else{
+                        header("Content-Type: application/json");
+                        echo json_encode(['error' => 'endpoint does not exist']);
+                        break;
+                    }
+                }
+                else{
+                    require __DIR__.'/../../public/pages/404.html';
+                    break;
+                }
             default:
-                echo json_encode(['error' => "endpoint does not exist"]);
+                require __DIR__.'/../../public/pages/404.html';
                 break;
         }
     }
--- public/index.php
+++ public/index.php
@@ -7,20 +7,17 @@ $dotenv = Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 $parts = explode("/", $_SERVER['REQUEST_URI']);
 $router = new Router();
+
 if(empty($parts[1])){
     http_response_code(302);
     header("Location: /home");
+    exit;
 }
+
 else{
-    if($parts[1] == 'api'){
-        header("Content-type: application/json; charset=UTF-8");
-        $path = $parts[2];
-        $id = $parts[3] ?? null;
-        $router->dispatch($_SERVER['REQUEST_METHOD'], $path, $id);
-    }
-    else{
-        $path = $parts[1];
-        $id = $parts[2] ?? null;
-        $router->dispatch($_SERVER['REQUEST_METHOD'], $path, $id);
-    }
+    $path1 = $parts[1] ?? null;
+    $path2 = $parts[2] ?? null;
+    $id    = $parts[3] ?? null;
+    $router->dispatch($_SERVER['REQUEST_METHOD'], $path1, $path2, $id);
+    exit;
 }
