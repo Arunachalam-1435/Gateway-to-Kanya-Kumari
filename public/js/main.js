75197efb20a60d2b618d44253f39a0fe028b83ef
Arunachalam-1435 <karunachalam120@gmail.com>
2026-04-13 11:36:33 +0530

    Map bug fixed

--- public/js/main.js
+++ public/js/main.js
@@ -28,7 +28,7 @@ function success(position) {
     var m = document.getElementById("map");
     m.setAttribute("style", "height: 180px");
     m.innerHTML = "";
-    currentMap = L.map('map').set([lat, lon], 12);
+    currentMap = L.map('map').setView([lat, lon], 12);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
